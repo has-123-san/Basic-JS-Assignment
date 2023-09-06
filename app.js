533f484fb40533da59563b2c259e360f2ec0fb44
has-123-san <hassanhanif2345@gmail.com>
2023-09-06 23:36:05 +0500

    commit done

---
+++ app.js
@@ -0,0 +1,526 @@
+
+// Chapter No 1
+
+
+// Question 1
+
+alert(
+    "Hello Users"
+)
+
+
+
+// Question 2
+
+alert(
+    "Error! Please enter a valid passsword."
+)
+
+
+// Question 3
+
+
+alert(
+    "Welcome to JS Land... \nHappy Coding!"
+)
+
+
+// Question 4
+
+
+alert(
+    "Welcome to JS Land... "
+)
+
+
+alert(
+    "Happy Coding!"
+)
+
+
+
+// Question 5           
+
+alert(
+    "Hello... I can run JS through my web browser's console"
+)
+
+
+// Chapter No 1 Completed 
+
+
+
+
+// Chapter No 2
+
+
+// Question 1
+
+var username;
+
+
+// Question 2
+
+var myName = "Muhammad Hassan"
+
+
+// Question 3
+
+var message;
+
+message = "Hello World"
+
+alert(message)
+
+
+// Question 4
+
+
+// Students Bio
+
+
+var studentName;
+ 
+studentName = "Jhone Doe"
+
+alert(studentName)
+
+
+
+
+var studentAge; 
+
+studentAge = "15 years old"
+
+alert(studentAge)
+
+
+var studentQualificaion
+
+studentQualificaion = "Certified Mobile Application Development"
+
+alert(studentQualificaion)
+
+
+
+
+// Question 5
+
+alert(
+    "PIZZA \nPIZZ \nPIZ \nPI \nP" 
+)
+
+
+
+// Question 6
+
+var email = "My email address is example@gmail.com"
+
+alert(email)
+
+
+
+// Question 7
+
+var book = "A smarter way to learn JavaScript"
+
+alert(book)
+
+
+
+// Question 8
+
+document.write("Yah! I can write HTML content through JavaScript")
+
+
+
+// Question 9
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
+
+
+// Chapter No 2 Completed
+
+
+
+// Chapter No 3 
+
+
+// Question 1
+
+var age ="I am 15 years old"
+
+alert(age)
+
+
+// Question 2
+
+var visited_time = "You have visited this site 15 times"
+
+alert(visited_time)
+
+
+
+// Question 3
+
+var birthYear = "<br> <br>  My birth year is 2005 <br> Data type of any declared variable is number <br> <br>"
+
+document.write(birthYear)
+
+
+
+
+// Question 4
+
+var visiterName = "John Doe"
+
+var product_title = "T-shirt"
+
+var Quantity = 5
+
+
+document.write(visiterName + " ordered " + Quantity + product_title + "(s) on XYZ Clothing store.<br> <br> "  )
+
+
+// Chapter No 3 Completed
+
+
+
+// Chapter No 4
+
+
+// Question 1
+
+var var1 = 10,  var2 = "Hello",  var3 = true; 
+
+document.write(var1 +"   "+ var2 +"   "+ var3)
+
+
+
+// Question 2
+
+document.write("<br><br> <h1> Legal Variable Name </h1>")
+
+var myVariable = "myVariable"
+
+var _value = "_value"
+
+var counter123 = "counter123"
+
+var $price = "$price"
+
+var CamelCaseExample = "CamelCaseExample"
+
+document.write("<h4> <br>1.  myVariable <br>2.  _value <br>3.  counter123 <br>4.  $price <br>5.  CamelCaseExample </h4> ")
+
+
+
+
+document.write("<br><br> <h1> Illegal Variable Name </h1> <br>")
+
+var firstIllegal = "<h4>  1.  my-Variable </h4> (Variables names cannot contain hyphens)"
+
+var second_illegal = "<h4>  2.  123variable </h4> (Variables names cannot start with a digit) "
+
+var Illegal_3 = "<h4> 3.   var </h4>  (Reserved Keywords cannot be used as variables name)"
+
+var $illegal = "<h4>  4.  my variables  </h4> (Variables names cannot have spaces) "
+
+var fifthIllegal = "<h4> 5.  @special  </h4> (Variables names cannot start with special characters like  @) "
+
+document.write(firstIllegal)
+
+document.write(second_illegal)
+
+document.write(Illegal_3)
+
+document.write($illegal)
+
+document.write(fifthIllegal)
+
+
+
+// Question 3
+
+var Head_ing = "<h2> Rules for naming JS variables </h2> "
+
+var Answer2 = "Variable names can only contain  numbers, Letters, $ and _. <br>For example $this_is2ndVariable <br> <br>"
+
+var Answer3 = "Variables must begin with a Letters, $ or_. <br>For example $word, _head or name <br> <br>"
+
+var Answer4 = "Variable names are case sensitive <br> <br>"
+
+var Answer5 = "Variable names should not be JS Keywords <br> "
+
+
+document.write(Head_ing)
+
+document.write(Answer2)
+
+document.write(Answer3)
+
+document.write(Answer4)
+
+document.write(Answer5)
+
+
+
+// Chapter No 4 Completed
+
+
+
+
+// Chapter No 5 
+
+
+// Question 1
+
+var num = 3
+
+var add = num + 5
+
+document.write("<br> <br> <br>Sum of 3 and 5 is "+ add)
+
+
+
+// Question 2
+
+
+var num = 8
+
+var subtract = num - 5
+
+document.write("<br> 8 - 5 = "+ subtract)
+
+
+var num = 5
+
+var Multiply = num * 3
+
+document.write("<br> 5 * 3 = "+ Multiply)
+
+
+
+var num = 27
+
+var Modulus = num % 4
+
+document.write("<br> 25 % 4 = "+ Modulus +"<br><br>")
+
+
+
+// Question 3
+
+var undefine;
+
+document.write("Value after variable declaration is "+ undefine)
+
+
+
+var number = 10
+
+document.write("<br> Initial value: "+number)
+
+
+
+var increment = ++number 
+
+document.write("<br> Value after increment is: "+increment)
+
+
+
+var addition = increment + 4
+
+document.write("<br> Value after addition is: "+addition)
+
+
+
+var decrement = addition - 5 
+
+document.write("<br> Value after decrement is: "+decrement)
+
+
+var divid = decrement % 2 
+
+document.write("<br> The remainder is : "+divid)
+
+
+
+
+// Question 4
+
+var ticket_price = 800
+
+var ticket_quantity = 7
+
+var ticket_result = ticket_price * ticket_quantity
+
+document.write(" <br> <br> Total cost to buy "+ ticket_quantity + " tickets to a movie is "+ ticket_result +" PKR")
+
+
+
+
+
+// Question 5
+
+
+
+var one = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9, ten = 10
+
+
+var nine_two = nine*two,  nine_three = nine*three,  nine_four = nine*four,  nine_five = nine*five
+
+
+var nine_six = nine*six,  nine_seven = nine*seven,  nine_eight = nine*eight,  nine_nine = nine*nine,  nine_ten = nine*ten
+
+
+
+document.write("<br><br> Table of "+nine)
+
+document.write("<br>" + nine + "*" + one + "=" + nine )
+
+document.write("<br>" + nine + "*" + two + "=" + nine_two )
+
+document.write("<br>" + nine + "*" + three + "=" + nine_three )
+
+document.write("<br>" + nine + "*" + four + "=" + nine_four )
+
+document.write("<br>" + nine + "*" + five + "=" + nine_five )
+
+document.write("<br>" + nine + "*" + six + "=" + nine_six )
+
+document.write("<br>" + nine + "*" + seven + "=" + nine_seven )
+
+document.write("<br>" + nine + "*" + eight + "=" + nine_eight )
+
+document.write("<br>" + nine + "*" + nine + "=" + nine_nine )
+
+document.write("<br>" + nine + "*" + ten + "=" + nine_ten )
+
+
+
+// Question 7
+
+
+var heading_shopping = "<br> <h2>Shopping Cart </h2> <br><br>"
+
+document.write(heading_shopping)
+
+var PriceOne = 850
+
+var PriceTwo = 1350
+
+var QuantityOne = 4
+
+var QuantityTwo = 3
+
+var ShippingCharges = 200
+
+var Shopping_result = PriceOne * QuantityOne + PriceTwo * QuantityTwo + ShippingCharges 
+
+document.write(`Price of item 1 is ${PriceOne}<br>`)
+
+document.write(`Quantity of item 1 is ${QuantityOne}<br>`)
+
+document.write(`Price of item 2 is ${PriceTwo}<br>`)
+
+document.write(`Quantity of item 2 is ${QuantityTwo}<br>`)
+
+document.write("Shipping Charges " +ShippingCharges)
+
+document.write("<br><br>Total cost of your order is "+Shopping_result )
+
+
+// Question 8
+
+var heading_marks = "<br> <h2>Marks Sheet </h2> <br>"
+
+var total_marks = 940
+
+var obtained_marks = 712
+
+var marks_result = obtained_marks * 100 / total_marks
+
+document.write(heading_marks)
+
+document.write("<br>Total marks: "+total_marks)
+
+document.write(`<br>Obtianed marks: ${obtained_marks}`)
+
+document.write(`<br>Percentage: ${marks_result}%`)
+
+
+
+// Question 9
+
+var US_Dollars_1 = 307,  Saudi_Riyal_1 = 81.85
+
+document.write("<br> <h2>Currency in PKR </h2>")
+
+document.write("Total Currency in PKR: "+US_Dollars_1 * Saudi_Riyal_1)
+
+
+
+// Question 10
+
+var number_result = 9 + 5 * 10 / 2
+
+document.write("<br><br> <h3>Question No 10 </h3> Result is "+number_result)
+
+
+
+
+// Question 11
+
+var heading_calculator = "<h2>Age Calculator</h2>"
+
+var Current_year = 2023
+
+var birth_year = 1998
+
+var age_result = Current_year - birth_year
+
+document.write(heading_calculator)
+
+document.write("Current Year: "+Current_year)
+
+document.write("<br>Birth Year: "+birth_year)
+
+document.write("<br>Age : "+age_result)
+
+
+// Question 12
+
+var heading_lifetime = "<h1>Lifetime Supply</h1>"
+
+var favorite_snack = "Milk Pack"
+
+var current_age = 18
+
+var maximum_age = 70
+
+var  per_day = 2
+
+var semi_result = maximum_age-current_age
+
+var final_result = semi_result * per_day
+
+document.write(heading_lifetime)
+
+document.write("Favorite Snack: "+favorite_snack)
+
+document.write("<br> Current Age: "+current_age)
+
+document.write("<br>Estimated Maximum Age: "+maximum_age)
+
+document.write("<br> Amount of snacks per day: "+per_day)
+
+document.write("<br> You will need "+ final_result +" "+ favorite_snack +" to last you until the ripe old age of "+ maximum_age)
+
+
+
+// Chapter No 5 Completed
+
+// Assignment Completed 
